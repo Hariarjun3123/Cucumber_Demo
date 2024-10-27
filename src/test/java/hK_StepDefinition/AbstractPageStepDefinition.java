43d40337ff6cd6b341648f1dda13eb0c4972bd7b
Hariarjun2331 <hariarjun2331@gmail.com>
2024-10-27 12:53:44 +0530

    WebDriver Manager dependency added

--- src/test/java/hK_StepDefinition/AbstractPageStepDefinition.java
+++ src/test/java/hK_StepDefinition/AbstractPageStepDefinition.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class AbstractPageStepDefinition {
 
 	protected static WebDriver driver;
